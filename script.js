bc05292d4bdafb4f8e6b4f058038ea15667728a6
Isaac Rakstad <iscrakdev@gmail.com>
2023-11-10 11:36:13 -0600

    added random color generation to [rpject

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 let selectedColor = "red";
 let previousColor = document.querySelector("#defaultColor");
 
+const generateRandomColor = () =>
+  Math.floor(Math.random() * 16777215).toString(16);
+const invertColor = (colorHex) =>
+  (Number(`0x1${colorHex}`) ^ 0xffffff).toString(16).substr(0, 6);
+
+// Sets the background and text color of an DOM node
+const assignRandomColor = function (node) {
+  const color = generateRandomColor();
+  node.style.backgroundColor = "#" + color;
+};
+
 function generateColorPallete() {
+  for (let i = 0; i < 13; i++) {
+    const pallete = document.querySelector('.pallete')
+    const newColorItem = document.createElement('div')
     
+    newColorItem.classList.add('grid-item', 'color')
+    assignRandomColor(newColorItem)
+    
+    newColorItem.addEventListener("click", (e) => {
+        const div = e.target;
+        selectedColor = div.style.backgroundColor;
+        
+        previousColor.classList.remove("selectedColor");
+        
+        div.classList.add("selectedColor");
+        
+        previousColor = div;
+    });
+
+    pallete.append(newColorItem)
+  }
 }
 
 function addCanvas() {
@@ -16,11 +46,11 @@ function addCanvas() {
 }
 
 function wipeCanvas() {
-    const canvas = document.querySelector(".canvas");
-    for (let box of canvas.children) {
-        box.style.backgroundColor = 'white'
-    }
+  const canvas = document.querySelector(".canvas");
+  for (let box of canvas.children) {
+    box.style.backgroundColor = "white";
   }
+}
 
 function addListeners() {
   const canvasItems = document.querySelector(".canvas").children;
@@ -28,37 +58,43 @@ function addListeners() {
 
   for (let item of canvasItems) {
     item.addEventListener("mousedown", (e) => {
-        const div = e.target;
-        div.style.backgroundColor = selectedColor;
+      const div = e.target;
+      div.style.backgroundColor = selectedColor;
     });
   }
-  
+
   for (let item of canvasItems) {
     item.addEventListener("mouseover", (e) => {
       if (e.buttons) {
         const div = e.target;
-      
+
         div.style.backgroundColor = selectedColor;
       }
     });
   }
 
-
   for (let item of palleteItems) {
     item.addEventListener("click", (e) => {
       const div = e.target;
       selectedColor = div.style.backgroundColor;
-      
+
       previousColor.classList.remove("selectedColor");
-      
+
       div.classList.add("selectedColor");
 
       previousColor = div;
     });
   }
 
-  let button = document.querySelector('#resetButton')
-  button.addEventListener('click', e => {wipeCanvas()})
+  const resetButton = document.querySelector("#resetButton");
+  const generateButton = document.querySelector("#generateColorButton");
+  
+  resetButton.addEventListener("click", (e) => {
+    wipeCanvas();
+  });
+  generateButton.addEventListener("click", (e) => {
+    generateColorPallete();
+  });
 }
 
 addCanvas();
